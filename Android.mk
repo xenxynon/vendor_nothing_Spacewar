df24e100e0d5c1ff83ee3e6d147314e429488f23
xenxynon <flynryder427@gmail.com>
2025-03-12 07:10:33 +0000

    Spacewar: regenerate makefiles for firmware
    Change-Id: Ib9bf7b81cf4c6820ea62e0bdf08a4bacc8cb9d13

--- Android.mk
+++ Android.mk
@@ -3,3 +3,26 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),Spacewar)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,022582a1b2326597832b7bcd78cec7ff4ef43893)
+$(call add-radio-file-sha1-checked,radio/aop.img,ef038c7dc44921e36ee379d55d9873491536141b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,62ec1de7624ead47f2ec41f03f4139430b463135)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,78e1130cb830bab7524d1b3bdd133510f2467d4e)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,da933faabec5e35efef34181ce3c7049e8e09d1a)
+$(call add-radio-file-sha1-checked,radio/dsp.img,afe65f6671c989fc2ee30c52c6ed9cb797f1d5cc)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,98748e403d7923a27f490c9f3c2b041a8e7df76a)
+$(call add-radio-file-sha1-checked,radio/hyp.img,e486ae720143f63ee3aef276ffbf97e178aa41be)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,9d143f5d45a35998ec2b0ad4a3438c99da47e84c)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,82a2bdf141e33966925ef7ab91d756d92c85ee7c)
+$(call add-radio-file-sha1-checked,radio/modem.img,07ac2239fd7e8dfbe82c249dc772fb1770d2b3bc)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,f30010b7ef292878181328eb6de18d879d73b350)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,26c265073fc4a933085c9447b1e42693eaa72720)
+$(call add-radio-file-sha1-checked,radio/shrm.img,0fb350b206c655f4bd8d218b94e6dc0ee5dcad2c)
+$(call add-radio-file-sha1-checked,radio/tz.img,6af9fa49acb9e674e5a00583488d17f9654da686)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,b9bb9b663e753b5fa87d69da1d02fa5cc367df0f)
+$(call add-radio-file-sha1-checked,radio/xbl.img,cc7d8c1f7c61f7a9d4667a0a6548d1f88639c54a)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,520bcc5105b3a0006d4f988e7cd079f1fd949390)
+
+endif
