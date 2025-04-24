d1826b17c83689f973ad29fbfeff4091f97c04f6
xenxynon <flynryder427@gmail.com>
2025-04-24 03:40:33 +0000

    Spacewar: nuke dirac
    Change-Id: I4e0619cf02e2cf9a595363775af5f963dbfb5419

--- Spacewar-vendor.mk
+++ Spacewar-vendor.mk
@@ -753,7 +753,6 @@ PRODUCT_PACKAGES += \
     libFileMux_proprietary \
     lib_lvacfs \
     libcppf \
-    libdirac \
     liblvacfs_wrapper \
     libmmrtpdecoder_proprietary \
     libmmrtpencoder_proprietary \
@@ -776,7 +775,6 @@ PRODUCT_PACKAGES += \
     libwfduibcsrcinterface_proprietary \
     libwfdutils_proprietary \
     libasphere \
-    libdiraceffect_NT \
     libshoebox \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
     com.qti.eeprom.abra_qtech_p24c128e_imx766_eeprom \
